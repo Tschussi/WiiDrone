d93c71fc3ddfcd60df568d0551ddfb644ac657b7
Kevin Hames <kevinrh1995@gmail.com>
2017-12-06 17:20:40 -0500

    Update wiiDrone.py

--- wiiDrone.py
+++ wiiDrone.py
@@ -10,6 +10,7 @@ MTR3 = 0x14 # Ch3 ..
 MTR4 = 0x24 # Ch7 ..
 INCREMENT = 10
 BUTTON_DELAY = 0.2
+HOVER = 1000
 ADDR = 0x40 # I2C address: sudo i2cdetect -y 1
 # Variables
 rightStopTime = STOP_LOWER_LIMIT
@@ -111,14 +112,70 @@ while True:
     wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
     accel = wii.state['acc']
     print(accel)
-    if accel[1] < 125:
-      print 'Increase Duty Cycle'
-    elif accel[1] > 135:
-      print 'Decrease Duty Cycle'
+    if accel[1] > 135:
+      if accel[1] > 142:
+        if accel[1] > 149
+          print 'Down Level 3'
+          rightStopTime = HOVER - 3 * INCREMENT
+          leftStopTime = HOVER - 3 * INCREMENT
+        else:
+          print 'Down Level 2'
+          rightStopTime = HOVER - 2 * INCREMENT
+          leftStopTime = HOVER - 2 * INCREMENT
+      else:
+        print 'Down Level 1'
+        rightStopTime = HOVER - INCREMENT
+        leftStopTime = HOVER - INCREMENT 
+    elif accel[1] < 125:
+      if accel[1] < 118:
+        if accel[1] < 111
+          print 'Up Level 3'
+          rightStopTime = HOVER + 3 * INCREMENT
+          leftStopTime = HOVER + 3 * INCREMENT
+        else:
+          print 'Up Level 2'
+          rightStopTime = HOVER + 2 * INCREMENT
+          leftStopTime = HOVER + 2 * INCREMENT
+      else:
+        print 'Up Level 3'
+        rightStopTime = HOVER + INCREMENT
+        leftStopTime = HOVER + INCREMENT 
+    else:
+      print 'Hover'
+      rightStopTime = HOVER
+      leftStopTime = HOVER
     if accel[0] < 125:
-      print 'Decrease Left Motors Duty Cycle'
+      if accel[0] < 118:
+        if accel[0] < 111:
+          print 'Left Level 3'
+          rightStopTime = rightStopTime + 3 * INCREMENT
+          leftStopTime = leftStopTime - 3 * INCREMENT
+        else:
+          print 'Left Level 2'
+          rightStopTime = rightStopTime + 2 * INCREMENT
+          leftStopTime = leftStopTime + 2 * INCREMENT
+      else:
+        print 'Left Level 1'
+        rightStopTime = rightStopTime + INCREMENT
+        leftSTOPTIME = leftStopTime + INCREMENT
     elif accel[0] > 135:
-      print 'Decrease Right Motors Duty Cycle'
+      if accel[0] > 142:
+        if accel[0] > 149:
+          print 'Right Level 3'
+          rightStopTime = rightStopTime - 3 * INCREMENT
+          leftStopTime = leftStopTime + 3 * INCREMENT
+        else:
+          print 'Right Level 2'
+          rightStopTime = rightStopTime - 2 * INCREMENT
+          leftStopTime = leftStopTime + + * INCREMENT
+      else:
+        print 'Right Level 1'
+        rightStopTime = rightStopTime - INCREMENT
+        leftSTOPTIME = leftStopTime + INCREMENT
+    bus.write_word_data(ADDR, MTR1, leftStopTime)
+    bus.write_word_data(ADDR, MTR2, leftStopTime)
+    bus.write_word_data(ADDR, MTR3, rightStopTime)
+    bus.write_word_data(ADDR, MTR4, rightStopTime)    
     time.sleep(BUTTON_DELAY)
 
   if (buttons & cwiid.BTN_B):
